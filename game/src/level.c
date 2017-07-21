3666991be740df728abb6c445224866c0b105d83
firoball <unsinnspala@firoball.de>
2017-07-22 01:00:15 +0200

    cleanup level.c

--- game/src/level.c
+++ game/src/level.c
@@ -1,26 +1,30 @@
 #include "camera.h"
 #include "player.h"
 
+var LEVEL__running = 0;
+
 void LEVEL_start()
 {
+	LEVEL__running = 1;
 	//level_load("prototype.mdl");
 	level_load("test_level_small.wmb");
 	player_init();
 	//setup camera	
 	focus_camera(player);
 	show_camera();
-	
-	//player = me;
 }
 
 void LEVEL_end()
 {
+	LEVEL__running = 0;
+	wait(1);
+	
 	hide_camera();
 }
 
 void LEVEL_loop()
 {
-	while(1)
+	while(LEVEL__running != 0)
 	{
 		player_move();
 		update_camera();
