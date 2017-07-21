587e69f0c96f71bf984b00c354da01dd49fc2aaf
padmalcom <j.freiknecht@googlemail.com>
2017-07-22 00:27:19 +0200

    [Jonas] initial player movement

--- game/lottivstheworld.c
+++ game/lottivstheworld.c
@@ -6,8 +6,6 @@
 #include "player.h"
 #include "camera.h"
 
-#include "player.c"
-
 //#define DEBUG
 #ifdef DEBUG
 	#include <default.c>
@@ -16,6 +14,15 @@
 void main() {
 	d3d_antialias = 9;
 	wait(1);
-	
-	create_camera();	
+	level_load("level\\test_level_small.wmb");
+	camera.z = 600;
+	//camera.tilt = 315;
+	camera.tilt = 270;
+	player_init();
+	while(1) {
+		player_move();
+		camera.x = player.x;
+		camera.y = player.y;
+		wait(1);
+	}
 }
--- game/src/player.h
+++ game/src/player.h
@@ -1,6 +1,9 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+void player_move();
+void player_init();
+
 #include "player.c"
 
 #endif
