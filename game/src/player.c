fb9c0861894e55887729baa9b9c436870251f5c6
firoball <unsinnspala@firoball.de>
2017-07-22 02:20:24 +0200

    turn to mouse first version

--- game/src/player.c
+++ game/src/player.c
@@ -11,8 +11,24 @@ void player_move() {
 	if (key_d) {
 		player.y -=5 * time_step;
 	}
+	
+ 	if (mouse_mode > 0)	
+  	{ 
+    	mouse_pos.x = mouse_cursor.x;    
+    	mouse_pos.y = mouse_cursor.y;
+  	}
+	
+	VECTOR* mousepos = vector(mouse_pos.x, mouse_pos.y, 0);
+	VECTOR* playerpos = vector(screen_size.x * 0.5, screen_size.y * 0.5, 0);
+	VECTOR* dist = vec_sub(playerpos, mousepos);
+	ANGLE dir;
+	vec_to_angle(dir, dist);	
+	player.pan = -dir.pan;
+	
+	DEBUG_VAR(player.pan, 10);
 }
 
 void player_init() {
-	player = ent_create(SPHERE_MDL, vector(0,0,0), NULL);
+	player = ent_create(CUBE_MDL, vector(0,0,0), NULL);
+	mouse_mode = 4;
 }
