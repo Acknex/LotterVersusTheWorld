9535763a24f2185b0e0ab3a4b5dca8a15a18da04
Felix Queißner <felix@ib-queissner.de>
2017-07-22 22:20:16 +0200

    Adds more teleporter stuff.

--- game/src/entity_defs.h
+++ game/src/entity_defs.h
@@ -3,11 +3,13 @@
 #define type skill1
 #define health skill10
 #define damage skill11
+#define pause_control skill12
 
 #define weapon_type skill45
 #define weapon_cooldown skill43
 #define weapon_bouncing skill44
 #define weapon_granade_cooldown skill46
+
 
 //skill20-40: for individual use, do not define here
 
--- game/src/teleporter.c
+++ game/src/teleporter.c
@@ -5,15 +5,26 @@ action teleporter_out()
 {
 	set(me, PASSABLE);
 	while(!player) wait(1);
+	
+	var portloader = 0;
 	while(me)
 	{
 		var dist = vec_dist(player.x, me.x);
-		DEBUG_VAR(dist, 512);
 		if(dist < 170) // ist höhenabhängig!
 		{
-			INIT_levelEnd();
-			return;
+			portloader += time_step;
+			if(portloader >= 100) {
+				INIT_levelEnd();
+				return;
+			}
 		}
+		else 
+		{
+			portloader = maxv(0, portloader - 10 * time_step);
+		}
+		DEBUG_VAR(dist, 512);
+		DEBUG_VAR(portloader, 528);
+		my->skill41 = floatv(0.001 + 0.001 * portloader);
 		wait(1);
 	}
 }
