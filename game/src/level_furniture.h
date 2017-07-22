a051afaf31e6562fdadec99ccb0b85099bfda827
BuildTools <unconfigured@null.spigotmc.org>
2017-07-22 23:27:37 +0200

    Weiter Möbel fixen.

--- game/src/level_furniture.h
+++ game/src/level_furniture.h
@@ -1,6 +1,13 @@
 #ifndef LEVEL_FURNITURE_H
 #define LEVEL_FURNITURE_H
 
+action screenOnWall()
+{
+	my->skill41 = floatv(1);
+	screen->material = BlinkingObjectMaterial;
+	set(screen, PASSABLE);
+}
+
 action rack_tapewheel()
 {
 	my->roll = random(360);
@@ -14,6 +21,7 @@ action rack_tapewheel()
 action rack_buildup()
 {
 	wait(1);
+	my->skill41 = floatv(1);
 	my->pan = my->skill1;
 	set(my, PASSABLE);
 	my->material = ObjectMaterial;
@@ -29,14 +37,17 @@ action rack_buildup()
 			{
 				model = ent_create("rack_taperec.mdl", _vec(my->x, my->y, my->z + z), 0);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				model = ent_create("rack_taperec_wheel.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(50, 14, z + 20), _vec(my->skill1, 0, 0))), rack_tapewheel);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				model = ent_create("rack_taperec_wheel.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(50, -14, z + 20), _vec(my->skill1, 0, 0))), rack_tapewheel);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				z += 40;
@@ -46,10 +57,12 @@ action rack_buildup()
 			{
 				model = ent_create("rack_console.mdl", _vec(my->x, my->y, my->z + z), 0);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				model = ent_create("rack_console_keyboard.mdl", _vec(my->x, my->y, my->z + z), 0);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				z += 40;
@@ -62,6 +75,7 @@ action rack_buildup()
 			{
 				model = ent_create("rack_hdd.mdl", _vec(my->x, my->y, my->z + z), 0);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				z += 20;
@@ -71,6 +85,7 @@ action rack_buildup()
 			{
 				model = ent_create("rack_memory.mdl", _vec(my->x, my->y, my->z + z), 0);
 				model->pan = my->skill1;
+				model->skill41 = floatv(1);
 				model->material = BlinkingObjectMaterial;
 				set(model, PASSABLE);
 				z += 20;
@@ -87,26 +102,31 @@ action desk_buildup()
 	set(my, PASSABLE);
 	ENTITY* keyboard = ent_create("rack_console_keyboard.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(-11, -3, 68), _vec(my->skill1, 0, 0))), 0);
 	keyboard->pan = -5 + my->skill1;
+	keyboard->skill41 = floatv(1);
 	keyboard->material = BlinkingObjectMaterial;
 	set(keyboard, PASSABLE);
 
 	ENTITY* screen = ent_create("desk_screen.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(20, 10, 84), _vec(my->skill1, 0, 0))), 0);
 	screen->pan = -15 + my->skill1;
+	screen->skill41 = floatv(1);
 	screen->material = BlinkingObjectMaterial;
 	set(screen, PASSABLE);
 
 	ENTITY* lamp = ent_create("desk_lamp.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(20, -50, 70), _vec(my->skill1, 0, 0))), 0);
 	lamp->pan = 50 + my->skill1;
+	lamp->skill41 = floatv(1);
 	lamp->material = ObjectMaterial;
 	set(lamp, PASSABLE);
 
 	ENTITY* phone = ent_create("desk_phone.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(20, -80, 80), _vec(my->skill1, 0, 0))), 0);
 	phone->pan = 20+ my->skill1;
+	phone->skill41 = floatv(1);
 	phone->material = ObjectMaterial;
 	set(phone, PASSABLE);
 
 	ENTITY* rack = ent_create("rack_case.mdl", vec_add(_vec(my->x, my->y, my->z), vec_rotate(_vec(0, 65, 0), _vec(my->skill1, 0, 0))), rack_buildup);
 	rack->skill1 = my->skill1;
+	rack->skill41 = floatv(1);
 	rack->material = ObjectMaterial;
 	set(rack, PASSABLE);
 }
