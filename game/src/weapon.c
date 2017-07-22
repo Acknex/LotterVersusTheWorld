2488efaced650e7a7a7ea0a6362225a80f6fcae0
Patrick <patrick.jendral@gmail.com>
2017-07-22 17:54:01 +0200

    Damage

--- game/src/weapon.c
+++ game/src/weapon.c
@@ -5,6 +5,7 @@ void weapon_startup()
 		if((player != NULL) && mouse_left) 
 		{
 			player.skill44 = 2;
+			player.group = 3;
 			shoot(1);
 		}
 		else if((player != NULL) && mouse_right)
@@ -55,6 +56,7 @@ void projectile()
 	
 	my.flags |= (PASSABLE | FLAG2);
 	my.type = TypePlayerProjectile;
+	my.damage = 1;
 	
 	VECTOR dir;
 	VECTOR offset;
@@ -69,8 +71,12 @@ void projectile()
 	my.tilt = 90;
 	my.pan += 90;
 	
+	
+	
 	vec_scale(my.scale_x, weapon_projectile_scale);
 	c_setminmax(me);
+	
+	
 	
 	my.skill50 = 0; // How many time a projectile has bounced already
 	
@@ -91,6 +97,7 @@ void projectile()
 		vec_add(to, my.x);
 		//vec_scale(offset, -16);
 		//vec_add(offset, my.x);
+		c_ignore(3);
 		dist = c_trace(my.x, to, IGNORE_ME | IGNORE_PASSABLE | ACTIVATE_SHOOT);
 		
 		
@@ -181,6 +188,8 @@ void granate()
 	vec_lerp(midPos2,vstart,vTarget,0.667);
 	midPos2.z = 200;
 	
+	my.damage = 3;
+	
 	// Bezier interpolation
 	while(my.skill1 < 16 && me)
 	{
@@ -205,17 +214,10 @@ void granate()
 		c_move(me, nullvector, temp, IGNORE_ME | IGNORE_PASSABLE | IGNORE_PUSH);
 		wait(1);
 	}
-	explosion(me);
+	//explosion(me);
 	c_scan(my.x, nullvector, vector(360, 0, 16), ACTIVATE_SHOOT);
 	ent_remove(me);
 }
-
-void shotgun()
-{
-	
-}
-
-
 
 void cooldown()
 {
