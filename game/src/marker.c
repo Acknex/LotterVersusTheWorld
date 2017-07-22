dd1caff3f94aeec85d17599008d9ba2955ee79c0
firoball <unsinnspala@firoball.de>
2017-07-22 14:34:01 +0200

    marker configuration

--- game/src/marker.c
+++ game/src/marker.c
@@ -1,4 +1,5 @@
 #include "camera.h"
+#include "marker_cfg.h"
 
 void MARKER_attach(ENTITY* ent)
 {
@@ -27,9 +28,11 @@ void MARKER_update(ENTITY* ent)
 		VIEW* view = get_camera();
 		VECTOR vecTemp;
 		vec_set(&vecTemp, ent->x);
-		if (vec_to_screen(vecTemp, get_camera()) != NULL)
+		if ((vec_to_screen(vecTemp, get_camera()) != NULL) && (ent->type < txtMarkers->strings))
 		{
-			draw_text(".jetpack\n 0xC0FFEE", vecTemp.x, vecTemp.y, vector(255,255,255));
+			STRING* str = (txtMarkers->pstring)[ent->type];
+			//draw_text(".jetpack\n 0xC0FFEE", vecTemp.x, vecTemp.y, vector(255,255,255));
+			draw_text(str, vecTemp.x, vecTemp.y, vector(255,255,255));
 		}
 	}	
 }
