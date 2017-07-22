c97e86afbad09d502dd85363976fa8b21a1c69b4
padmalcom <j.freiknecht@googlemail.com>
2017-07-22 02:58:37 +0200

    [Jonas] player movement... mal wieder

--- game/src/player.c
+++ game/src/player.c
@@ -1,16 +1,10 @@
+var anim_percentage = 0;
+var dist_ahead = 0;
+var dist_strafe = 0;
+VECTOR *mousepos, *playerpos, *dist;
+ANGLE dir;
+
 void player_move() {
-	if (key_w) {
-		player.x +=5 * time_step;
-	}
-	if (key_s) {
-		player.x -=5 * time_step;
-	}
-	if (key_a) {
-		player.y +=5 * time_step;
-	}
-	if (key_d) {
-		player.y -=5 * time_step;
-	}
 	
  	if (mouse_mode > 0)	
   	{ 
@@ -18,17 +12,24 @@ void player_move() {
     	mouse_pos.y = mouse_cursor.y;
   	}
 	
-	VECTOR* mousepos = vector(mouse_pos.x, mouse_pos.y, 0);
-	VECTOR* playerpos = vector(screen_size.x * 0.5, screen_size.y * 0.5, 0);
-	VECTOR* dist = vec_sub(playerpos, mousepos);
-	ANGLE dir;
+	mousepos = vector(mouse_pos.x, mouse_pos.y, 0);
+	playerpos = vector(screen_size.x * 0.5, screen_size.y * 0.5, 0);
+	dist = vec_sub(playerpos, mousepos);
 	vec_to_angle(dir, dist);	
 	player.pan = -dir.pan;
 	
-	DEBUG_VAR(player.pan, 10);
+	DEBUG_VAR(player.pan, 100);
+	
+	
+	dist_ahead = (PLAYER_WALK_SPPED + key_shiftl*PLAYER_RUN_SPEED) * (key_w - key_s) * time_step;
+	dist_strafe = (PLAYER_WALK_SPPED + key_shiftl*PLAYER_RUN_SPEED) * (key_a - key_d) * time_step;
+	VECTOR* vecDir = vector(dist_ahead, dist_strafe, 0);
+	c_move(player, nullvector, vecDir, IGNORE_PASSABLE | GLIDE);
 }
 
 void player_init() {
 	player = ent_create(CUBE_MDL, vector(0,0,0), NULL);
+	vec_scale(player.scale_x, 3);
+	c_setminmax(player);
 	mouse_mode = 4;
 }
--- game/src/player.h
+++ game/src/player.h
@@ -1,6 +1,9 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#define PLAYER_WALK_SPPED 8
+#define PLAYER_RUN_SPEED 4
+
 void player_move();
 void player_init();
 
