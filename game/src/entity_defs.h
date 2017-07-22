4a0bb8d64cec56eec717d349fa501b839f3a3704
Patrick <patrick.jendral@gmail.com>
2017-07-22 14:18:33 +0200

    Mehr defs

--- game/src/entity_defs.h
+++ game/src/entity_defs.h
@@ -4,6 +4,10 @@
 #define health skill10
 #define damage skill11
 
+#define weapon_type skill45
+#define weapon_cooldown skill43
+#define weapon_bouncing skill44
+
 //skill20-40: for individual use, do not define here
 
 #define TypePlayerProjectile 1
