6ce072f6119aceaba730541fc5bceb2a22951979
mk1x86 <dev@mk-scape.de>
2017-07-22 11:28:51 +0200

    ricochet effect

--- game/src/weapon.c
+++ game/src/weapon.c
@@ -11,15 +11,32 @@ void weapon_startup()
 	}
 }
 
+#include "camera.h"
 void ricochet_effect()
 {
 	var t = 0;
-	while(t < 300) {
-		vec_scale(my.x, t/300*1.5);
-		my.alpha = (300 - time_step)/300;
-		t+=time_step/16;
+	my.flags |= LIGHT | PASSABLE | TRANSLUCENT;
+	my.red = 255;
+	my.green = 128;
+	my.blue = 255;
+	if(my.pan == 0)
+		my.pan = 0.01;
+	if(my.tilt == 0)
+		my.tilt = 0.01;
+	
+	vec_scale(my.scale_x, 0.5);
+	while(t < 0.3) {
+		var f = t / 0.3;
+		var scale = 0.01+f*1;
+		vec_set(my.scale_x, vector(scale,scale,scale));
+		my.alpha = (1-f)*100;
+		t += time_step/16;
+		DEBUG_VAR(t,150);
+		wait(1);
 	}
+	ptr_remove(my);
 }
+
 void projectile()
 {
 	var t = 0;
@@ -79,13 +96,20 @@ void projectile()
 		
 		if((dist != 0 || t > 5) && player.skill44 == 0 ) 
 		{ 
-			ENTITY* ricochet = ent_create("billboard.tga", my.x, ricochet_effect);
+			VECTOR* v = vector(hit.nx, hit.ny, hit.nz);
+			vec_normalize(v, 1);
+			vec_add(v.x, hit.x);
+			ENTITY* ricochet = ent_create("ricochet.tga", v, ricochet_effect);
 			vec_to_angle(ricochet->pan, vector(hit.nx, hit.ny, hit.nz));
+			//vec_add(ricochet->x, hit.nx);
 			break; 
 		}
 		else if( dist != 0 && player.skill44 > 0 && my.skill50 < player.skill44 )
 		{
-			ENTITY* ricochet = ent_create("billboard.tga", my.x, ricochet_effect);
+			VECTOR* v = vector(hit.nx, hit.ny, hit.nz);
+			vec_normalize(v, 1);
+			vec_add(v.x, hit.x);
+			ENTITY* ricochet = ent_create("ricochet.tga", v, ricochet_effect);
 			vec_to_angle(ricochet->pan, vector(hit.nx, hit.ny, hit.nz));
 			vec_set(dir, bounce);
 			vec_scale(dir, weapon_speed);
