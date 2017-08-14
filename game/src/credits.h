d1dcbf71db9a6ddc6e17ff2cc3c6e8294f9b1433
mk1x86 <dev@mk-scape.de>
2017-08-14 22:57:38 +0200

    credits speed

--- game/src/credits.h
+++ game/src/credits.h
@@ -2,7 +2,7 @@
 #define CREDITS_H
 
 STRING * credits_source = "credits.dat";
-var credits_scrollSpeed = 2.96; // 1 Pixel per quant
+var credits_scrollSpeed = 2.8; // 1 Pixel per quant
 var credits_speedup = 1;
 var credits_speedup_factor = 4;
 
